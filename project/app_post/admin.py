c99666e18858bd1ba79e2f508ec0af4869f2ee06
Oleg Diachenko <116761572+dichenk@users.noreply.github.com>
2023-06-12 05:08:29 +0300

    tasks 4 complited

--- project/app_post/admin.py
+++ project/app_post/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
 from .models import Post
+from django.urls import reverse
+from django.utils.html import format_html
 
-admin.site.register(Post)
+
+class PostAuthor(admin.ModelAdmin):
+    list_display = ('headline', 'author_link', 'creation_date')
+    list_filter = ('creation_date', )
+
+    def author_link(self, obj):
+        link_to_the_author = reverse('admin:app_user_customuser_change', args=[obj.author_id])
+        return format_html(f'<a href="http://127.0.0.1:8000/{link_to_the_author}">{obj.author}</a>')
+
+admin.site.register(Post, PostAuthor)
--- project/app_user/models.py
+++ project/app_user/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=1024, verbose_name='Login', unique=True)
     password = models.CharField(max_length=1024, verbose_name='Password', unique=True)
     email = models.CharField(max_length=100, verbose_name='Email', unique=True, null=False)
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, default='5555555')
     date_of_birth = models.DateField(verbose_name='date of birth', default='1971-06-28')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='user created')
     edit_date = models.DateTimeField(auto_now=True, verbose_name='user updated')
